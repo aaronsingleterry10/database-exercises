2d48196a93cde1ddea6697ead93b303d00b4b716
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-09 10:49:45 -0500

    refactored

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,11 +2,11 @@ USE employees;
 
 SELECT DISTINCT title FROM titles;
 
-SELECT DISTINCT last_name FROM employees WHERE last_name LIKE 'e%e';
+SELECT last_name FROM employees WHERE last_name LIKE 'e%e' GROUP BY last_name;
 
-SELECT DISTINCT first_name, last_name FROM employees WHERE last_name LIKE 'e%e';
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'e%e' GROUP BY first_name, last_name;
 
-SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
 # SELECT first_name, last_name FROM employees GROUP BY first_name, last_name;
 
