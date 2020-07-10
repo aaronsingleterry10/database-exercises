a3e4fb2d3f6fae0fcec95ab1e70503cca83910ae
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-10 15:11:12 -0500

    completed a couple of problems

---
+++ subqueries_exercises.sql
@@ -0,0 +1,25 @@
+USE employees;
+SELECT * FROM employees;
+# Find all the employees with the same hire date as employee 101010 using a subquery.
+
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE hire_date = (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = '101010'
+    );
+# 69 Rows
+SELECT * FROM titles;
+SELECT * FROM dept_emp;
+# Find all the titles held by all employees with the first name Aamod.
+SELECT title
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+    );
+# 314 total titles, 6 unique titles
+#
+# Find all the current department managers that are female.
