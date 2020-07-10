b9ec2236eb5de5f4440576420a20a0d1503f5f74
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-10 12:00:07 -0500

    completed problem

--- join_exercises.sql
+++ join_exercises.sql
@@ -52,9 +52,38 @@ USE employees;
 SELECT dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name) AS 'Department Manager'
 FROM dept_manager
 JOIN employees ON employees.emp_no = dept_manager.emp_no
-JOIN departments d on dept_manager.dept_no = d.dept_no
+JOIN departments on dept_manager.dept_no = departments.dept_no
 WHERE dept_manager.to_date = '9999-01-01';
 
 SELECT * FROM dept_manager;
 SELECT * FROM departments;
 SELECT * FROM employees;
+
+SELECT dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name) AS 'Department Manager'
+FROM dept_manager
+JOIN employees ON employees.emp_no = dept_manager.emp_no
+JOIN departments ON dept_manager.dept_no = departments.dept_no
+WHERE dept_manager.to_date = '9999-01-01' AND employees.gender = 'F';
+
+SELECT * FROM titles;
+SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_emp;
+
+SELECT t.title AS Title, COUNT(*) AS Count
+FROM departments
+JOIN dept_emp de on departments.dept_no = de.dept_no
+JOIN titles t on de.emp_no = t.emp_no
+WHERE departments.dept_name = 'Customer Service' AND t.to_date = '9999-01-01' AND de.to_date = '9999-01-01'
+GROUP BY title;
+select * from salaries;
+SELECT departments.dept_name AS 'Department Name', CONCAT(employees.first_name, ' ', employees.last_name) AS 'Name', salaries.salary
+FROM departments
+JOIN dept_manager dm on departments.dept_no = dm.dept_no
+JOIN employees ON employees.emp_no = dm.emp_no
+JOIN salaries ON salaries.emp_no = employees.emp_no
+WHERE salaries.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
+ORDER BY dept_name;
+
+
+
